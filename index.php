a642c65b1a15195e4f034a3517201b0d34d45d41
Joy Neop <joyneop@me.com>
2013-11-19 17:02:47 +0800

    .

--- index.php
+++ index.php
@@ -1,6 +1,6 @@
 <?php
 $curl = curl_init();
-curl_setopt($curl, CURLOPT_URL, 'http://joyneop.github.io/NeOPSTEP/home.html');
+curl_setopt($curl, CURLOPT_URL, 'https://raw.github.com/JoyNeop/NeOPSTEP/gh-pages/home.html');
 curl_setopt($curl, CURLOPT_HEADER, 0);
 curl_setopt($curl, CURLOPT_RETURNTRANSFER, 1);
 $data = curl_exec($curl);
